8c6edc66e35557394c5ee002d84873df12630485
ohseungyun <afrada123@likelion.org>
2022-04-10 21:15:25 +0900

    # 뉴 프로젝트 시작 파일 빌드

---
+++ backEnd/myScaeduler/src/main/java/we/graduationproj/myScaeduler/MyScaedulerApplication.java
@@ -0,0 +1,13 @@
+package we.graduationproj.myScaeduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyScaedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyScaedulerApplication.class, args);
+	}
+
+}
---
+++ backEnd/myScaeduler/src/test/java/we/graduationproj/myScaeduler/MyScaedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package we.graduationproj.myScaeduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyScaedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
