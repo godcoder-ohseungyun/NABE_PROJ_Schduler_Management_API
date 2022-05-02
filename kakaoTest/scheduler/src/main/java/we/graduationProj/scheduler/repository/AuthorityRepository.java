14e193e71a6ad9898bb8037c9fd810d3f8bf7641
ohseungyun <afrada123@gmail.com>
2022-05-02 16:40:46 +0900

    # 서버 정리 +개발 설계 명세 + readme 기능 명세 추가

--- kakaoTest/scheduler/src/main/java/we/graduationProj/scheduler/repository/AuthorityRepository.java
+++
@@ -1,8 +0,0 @@
-package we.graduationProj.scheduler.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import we.graduationProj.scheduler.entity.Authority;
-
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
