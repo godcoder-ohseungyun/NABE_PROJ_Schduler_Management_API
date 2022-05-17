9786bbc79cb10219982eaa6b57c7dfe841d019c1
ohseungyun <afrada123@gmail.com>
2022-05-17 19:52:36 +0900

    프론트 요구사항 반영 #1 초기 데이터 보강 유저객체에 일정리스트들 추가

--- Server/server/src/main/java/navi/server/config/TestDataInit.java
+++ Server/server/src/main/java/navi/server/config/TestDataInit.java
@@ -3,6 +3,10 @@ package navi.server.config;
 
 import lombok.RequiredArgsConstructor;
 import navi.server.domain.user.User;
+import navi.server.dto.announcementScheduleDTO.AddingAnDTO;
+import navi.server.dto.personalScheduleDTO.CreatingPsDTO;
+import navi.server.dto.specialScheduleDTO.AddingSpDTO;
+import navi.server.service.scheduler.SchedulerService;
 import navi.server.service.user.UserService;
 import org.springframework.stereotype.Component;
 
@@ -13,10 +17,23 @@ import javax.annotation.PostConstruct;
 public class TestDataInit {
 
     private final UserService userService;
+    private final SchedulerService schedulerService;
 
     @PostConstruct
     public void init(){
         userService.join(new User("비밀번호","tv0602@naver.com","오승윤")); //최초 가입자
+        User find = userService.findUserByUniqueId(0l);
+
+        schedulerService.createAnnouncementSchedule(find,new AddingAnDTO("23213993","test.com","테스트","테스트내용","2020-03-01","2020-04-01"));
+        schedulerService.createAnnouncementSchedule(find,new AddingAnDTO("55433321","tes2t.com","테스트2","테스트내용2","2020-03-01","2020-04-01"));
+
+        schedulerService.createPersonalSchedule(find,new CreatingPsDTO("2020-03-01","09:30","10:00",true,"테스트내용1"));
+        schedulerService.createPersonalSchedule(find,new CreatingPsDTO("2020-03-01","12:30","14:00",true,"테스트내용2"));
+
+        schedulerService.createSpecialSchedule(find,new AddingSpDTO("Opic","2020-03-01","2020-03-01","2020-03-01","2020-03-01"));
+        schedulerService.createSpecialSchedule(find,new AddingSpDTO("Toeic","2020-03-01","2020-03-01","2020-03-01","2020-03-01"));
 
     }
+
+
 }
