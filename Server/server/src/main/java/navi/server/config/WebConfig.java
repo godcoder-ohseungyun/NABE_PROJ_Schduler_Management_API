443164798d166528844c345e9771a976bfa30e98
ohseungyun <afrada123@gmail.com>
2022-07-03 16:47:54 +0900

    #?

---
+++ Server/server/src/main/java/navi/server/config/WebConfig.java
@@ -0,0 +1,23 @@
+package navi.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE_SECS = 3600;
+    /**
+     * CORS 설정
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") //8081로 부터 오는 모든 요청에 CORS 적용
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS); //preflight 결과를 3600초 동안 캐시에 저장
+    }
+}
