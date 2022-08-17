32dc721aeb18edab30a3dde69c97b02d9137b8d4
오승윤 <>
2022-08-17 14:16:56 +0900

    git action test
    실패 유도 테스트

---
+++ src/test/java/scheduler/api/service/ps/PersonalScheduleServiceTest.java
@@ -0,0 +1,27 @@
+package scheduler.api.service.ps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+import scheduler.api.domain.ps.PersonalSchedule;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class PersonalScheduleServiceTest {
+
+    @Autowired
+    PersonalScheduleService personalScheduleService;
+
+
+    @Test
+    public void test() throws Exception{
+        
+        assertEquals(1,2);
+    }
+}
