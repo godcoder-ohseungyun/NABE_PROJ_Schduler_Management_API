40963e4e3adfd878c32a240698d1cfd6b246a5d2
오승윤 <>
2022-08-17 17:09:02 +0900

    no message

--- src/test/java/scheduler/api/ApiApplicationTests.java
+++ src/test/java/scheduler/api/ApiApplicationTests.java
@@ -1,19 +1,31 @@
 package scheduler.api;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import scheduler.api.service.ps.PersonalScheduleServiceTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.transaction.annotation.Transactional;
+
+
 
 @SpringBootTest
+@Transactional
 public class ApiApplicationTests {
 
 	@Test
-	public void contextLoads() {
-		assertEquals(1,2);
+	public void test1() throws Exception {
+
+		assertEquals(1,1);
 	}
 
+	@Test
+	public void test2() throws Exception {
 
+		assertEquals(1,2);
+	}
 
 }
--- src/test/java/scheduler/api/service/ps/PersonalScheduleServiceTest.java
+++ src/test/java/scheduler/api/service/ps/PersonalScheduleServiceTest.java
@@ -1,28 +1,18 @@
 package scheduler.api.service.ps;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
-import scheduler.api.ApiApplication;
-import scheduler.api.ApiApplicationTests;
-import scheduler.api.domain.ps.PersonalSchedule;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
 public class PersonalScheduleServiceTest {
 
-    @Autowired
-    PersonalScheduleService personalScheduleService;
-
-
     @Test
-    public void test() {
-        throw new RuntimeException();
+    public void test3() throws Exception {
+
+        assertEquals(1,1);
     }
 }
