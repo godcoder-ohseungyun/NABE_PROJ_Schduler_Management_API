f3b1f0bdb0d8aa40002744b6abf21019842888a0
오승윤 <>
2022-11-25 22:54:02 +0900

    feat (JasyptConfig) : Jasypt를 활용하여 AWS RDS 연결 및 연결 정보 암호화
    application.yml에 선언되어야하는 Jasypt 복호화 키 환경 변수는 Git CICD시 동적 선언 되도록 할것, 커밋 로그에 환경 변수 값 노출 금지

---
+++ src/main/java/nabe/scheduler/api/config/JasyptConfig.java
@@ -0,0 +1,37 @@
+package nabe.scheduler.api.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * @detail
+ * - Jasypt설정: AWS RDS 연결을 위한 application.yml의 DB정보 암호화
+ * - 복호화키 {enviroment}는 git Actions CI/CD 동작 수행 중 application.yml에 생성됩니다.
+ */
+@Configuration
+public class JasyptConfig {
+    @Value("${enviroment}")
+    private String encryptKey;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(encryptKey);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+
+}
